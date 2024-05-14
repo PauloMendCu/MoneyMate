3f77083780471fd4d7096ce17ec3e03fa502d36b
Carlos Jesus <jesus-18_45@outlook.es>
2024-05-14 13:44:08 -0500

    Tarjeta de Credito

--- app/src/main/java/services/ICategoriaService.java
+++ app/src/main/java/services/ICategoriaService.java
@@ -3,10 +3,14 @@ package services;
 import java.util.List;
 
 import entities.Categoria;
+import entities.TarjetaCredito;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ICategoriaService {
     @GET("/Categoria")
     Call<List<Categoria>> getCategorias();
+
+    @GET("/Credito")
+    Call<List<TarjetaCredito>> getCreditos();
 }
